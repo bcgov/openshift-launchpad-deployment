c6838f50e5c13ebc91c47d01685c6b9919013f77
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-11 16:08:01 -0700

    Use implicit env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,23 @@
 cd /
 
 # Login to cluster
-oc login https://console.pathfinder.gov.bc.ca:8443 --token="$AUTH_TOKEN"
+oc login https://console.pathfinder.gov.bc.ca:8443 --token="$INPUT_AUTH_TOKEN"
 
 # Clear previous builds including secrets & volume claims
-make oc-all-clean NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
-make oc-persisted-clean NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
+make oc-all-clean NAMESPACE=$INPUT_NAMESPACE APP_NAME=$INPUT_APP_NAME
+make oc-persisted-clean NAMESPACE=$INPUT_NAMESPACE APP_NAME=$INPUT_APP_NAME
 
 # Create network security policies, takes time to be applied
-make create-nsp NAMESPACE=$NAMESPACE APP_NAME=$APP_NAME
+make create-nsp NAMESPACE=$INPUT_NAMESPACE APP_NAME=$INPUT_APP_NAME
 sleep 30s
 
 # Run relevant make command, uses MODE input to select
-case "$MODE" in
+case "$INPUT_MODE" in
   "server")
-    make create-server NAMESPACE="$NAMESPACE" APP_NAME="$APP_NAME" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest SERVER_PORT="$SERVER_PORT"
+    make create-server NAMESPACE="$INPUT_NAMESPACE" APP_NAME="$INPUT_APP_NAME" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest SERVER_PORT="$INPUT_SERVER_PORT"
     ;;
   "client")
-    make create-client NAMESPACE="$NAMESPACE" APP_NAME="$APP_NAME" API_URL="$API_URL" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest CLIENT_PORT="$CLIENT_PORT"
+    make create-client NAMESPACE="$INPUT_NAMESPACE" APP_NAME="$INPUT_APP_NAME" API_URL="$INPUT_API_URL" REPO="https://github.com/$GITHUB_REPOSITORY" BRANCH=master IMAGE_TAG=latest CLIENT_PORT="$INPUT_CLIENT_PORT"
     ;;
   *)
     echo "Must specify MODE as either client or server"
