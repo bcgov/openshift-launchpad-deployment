37a76273295215079701375b88e4e6ff69572423
Ashton Meuser <ashtonmeuser@gmail.com>
2020-03-23 09:08:26 -0700

    Accept script args

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,13 @@ set -xe # Exit on error
 # Files from consuming repo are located in $GITHUB_WORKSPACE
 cd /deployment
 
+# Login to OpenShift cluster
+if test -z "$INPUT_CLUSTER" || test -z "$INPUT_AUTH_TOKEN"; then # Ensure valid login
+  echo "Please provide CLUSTER and AUTH_TOKEN inputs"
+  exit 1
+fi
+oc login "$INPUT_CLUSTER" --token="$INPUT_AUTH_TOKEN"
+
 # If a custom script has been provided, run it
 case "$INPUT_SCRIPT" in
   "")
@@ -12,23 +19,17 @@ case "$INPUT_SCRIPT" in
     ;;
   *)
     SCRIPT_PATH="$GITHUB_WORKSPACE/$INPUT_SCRIPT"
-    if test -z "$GITHUB_WORKSPACE" || test ! -f "$SCRIPT_PATH"; then # Ensure script exists
-      echo "Unable to locate custom deployment script $SCRIPT_PATH"
+    SANS_ARGS=$(echo "$SCRIPT_PATH" | cut -d' ' -f1)
+    if test -z "$GITHUB_WORKSPACE" || test ! -f "$SANS_ARGS"; then # Ensure script exists
+      echo "Unable to locate custom deployment script $SANS_ARGS"
       exit 1
     fi
     echo "Running custom deployment script $SCRIPT_PATH"
     cd "$GITHUB_WORKSPACE"
-    "$SCRIPT_PATH"
+    eval "$SCRIPT_PATH"
     exit 0 # Successfully ran custom script
     ;;
 esac
-
-# Login to cluster
-if test -z "$INPUT_CLUSTER" || test -z "$INPUT_AUTH_TOKEN"; then # Ensure valid login
-  echo "Please provide CLUSTER and AUTH_TOKEN inputs"
-  exit 1
-fi
-oc login "$INPUT_CLUSTER" --token="$INPUT_AUTH_TOKEN"
 
 # Clear previous builds including secrets & volume claims
 make oc-all-clean NAMESPACE=$INPUT_NAMESPACE APP_NAME=$INPUT_APP_NAME
